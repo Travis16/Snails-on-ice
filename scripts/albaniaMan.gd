0c5fed8360b33c8f5ef4f1a5a12d397f86d989e1
Joe Williams <jw06saturn@gmail.com>
2025-02-10 00:41:55 +0000

    snail particle accelerator

--- scripts/albaniaMan.gd
+++ scripts/albaniaMan.gd
@@ -1,17 +1,39 @@
 extends Node2D
 
-
 var rng = RandomNumberGenerator.new()
-var speed;
+var xspeed;
+var yspeed;
+var colour;
+var dir;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	speed = rng.randf_range(0.5, 3)
+	xspeed = 0
+	yspeed = 0
+	colour = rng.randi_range(0,1)
+	if colour == 0:
+		$red.hide()
+	else:
+		$green.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y += speed;
-
+	pass
 	
 func _physics_process(delta: float) -> void:
-	pass
+	position.y += yspeed;
+	position.x += xspeed;
+	rotation_degrees += xspeed + yspeed
+	
+	if Input.is_action_pressed("up"):
+		dir = 0
+		yspeed -= 0.1
+	if Input.is_action_pressed("right"):
+		dir = 1
+		xspeed += 0.1
+	if Input.is_action_pressed("down"):
+		dir = 2
+		yspeed += 0.1
+	if Input.is_action_pressed("left"):
+		dir = 3
+		xspeed -= 0.1
--- scripts/gameRunner.gd
+++ scripts/gameRunner.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 var rng = RandomNumberGenerator.new()
 var new_albanian =  preload("res://scenes/albaniaMan.tscn")
+var dir = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,10 +14,32 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("up"):
+		dir = 0
+	if Input.is_action_pressed("right"):
+		dir = 1
+	if Input.is_action_pressed("down"):
+		dir = 2
+	if Input.is_action_pressed("left"):
+		dir = 3
+	
 func spawn_albanians() -> void:
 	var newAlbanianInstance = new_albanian.instantiate()
-	newAlbanianInstance.position.x = rng.randf_range(0, 1000)
-	newAlbanianInstance.position.y = -100
+	if dir == 0:
+		newAlbanianInstance.position.x = rng.randf_range(0, 1000)
+		newAlbanianInstance.position.y = 800
+	if dir == 1:
+		newAlbanianInstance.position.y = rng.randf_range(0, 500)
+		newAlbanianInstance.position.x = -100
+	if dir == 2:
+		newAlbanianInstance.position.x = rng.randf_range(0, 1000)
+		newAlbanianInstance.position.y = -100
+	if dir == 3:
+		newAlbanianInstance.position.y = rng.randf_range(0, 500)
+		newAlbanianInstance.position.x = 1300
+	
+	newAlbanianInstance.dir = dir
 	self.add_child(newAlbanianInstance)
 	
 
