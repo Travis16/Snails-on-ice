732f17241166803560a12c8235c2f8e063c1f8bc
Joe Williams <jw06saturn@gmail.com>
2025-02-19 21:26:54 +0000

    first draft of acceleration

--- scripts/albaniaMan.gd
+++ scripts/albaniaMan.gd
@@ -1,39 +1,39 @@
-extends Node2D
+extends CharacterBody2D
 
 var rng = RandomNumberGenerator.new()
-var xspeed;
-var yspeed;
 var colour;
-var dir;
+var speed = 0;
+const rotation_speed = 3;
+const max_speed = 1000000
+const min_speed = 0
+var forward_direction = Vector2.RIGHT
+var latest_collision
+var id
+var input_delay = 0.1
+var accel = 800
+var brake = 6000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	xspeed = 0
-	yspeed = 0
 	colour = rng.randi_range(0,1)
 	if colour == 0:
 		$red.hide()
 	else:
 		$green.hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 func _physics_process(delta: float) -> void:
-	position.y += yspeed;
-	position.x += xspeed;
-	rotation_degrees += xspeed + yspeed
+	forward_direction = Vector2.RIGHT.rotated(rotation)
 	
-	if Input.is_action_pressed("up"):
-		dir = 0
-		yspeed -= 0.1
-	if Input.is_action_pressed("right"):
-		dir = 1
-		xspeed += 0.1
-	if Input.is_action_pressed("down"):
-		dir = 2
-		yspeed += 0.1
-	if Input.is_action_pressed("left"):
-		dir = 3
-		xspeed -= 0.1
+	if Input.is_action_pressed("up" + id):
+		speed = clamp(speed + (accel * delta), min_speed, max_speed)
+	if Input.is_action_pressed("right" + id):
+		self.rotation_degrees += rotation_speed
+	if Input.is_action_pressed("down" + id):
+		speed = clamp(speed - (brake * delta), min_speed, max_speed)
+	if Input.is_action_pressed("left" + id):
+		self.rotation_degrees -= rotation_speed
+	
+	speed = clamp(speed - 0.5, min_speed, max_speed)
+	
+	latest_collision = move_and_collide(lerp(self.velocity, forward_direction*speed, delta*input_delay)) 
--- scripts/gameRunner.gd
+++ scripts/gameRunner.gd
@@ -1,47 +1,29 @@
 extends Node2D
 
 var rng = RandomNumberGenerator.new()
-var new_albanian =  preload("res://scenes/albaniaMan.tscn")
-var dir = 2
+var player_scene =  preload("res://scenes/albaniaMan.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Timer.start()
-	spawn_albanians()
+	var player1 = player_scene.instantiate()
+	player1.position.x = randf_range(100,1800)
+	player1.position.y = randf_range(100,900)
+	player1.id = "1"
+	self.add_child(player1)
+	
+	
+	var player2 = player_scene.instantiate()
+	player2.position.x = randf_range(100,1800)
+	player2.position.y = randf_range(100,900)
+	player2.id = "2"
+	self.add_child(player2)
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("up"):
-		dir = 0
-	if Input.is_action_pressed("right"):
-		dir = 1
-	if Input.is_action_pressed("down"):
-		dir = 2
-	if Input.is_action_pressed("left"):
-		dir = 3
+	pass	
 	
-func spawn_albanians() -> void:
-	var newAlbanianInstance = new_albanian.instantiate()
-	if dir == 0:
-		newAlbanianInstance.position.x = rng.randf_range(0, 1000)
-		newAlbanianInstance.position.y = 800
-	if dir == 1:
-		newAlbanianInstance.position.y = rng.randf_range(0, 500)
-		newAlbanianInstance.position.x = -100
-	if dir == 2:
-		newAlbanianInstance.position.x = rng.randf_range(0, 1000)
-		newAlbanianInstance.position.y = -100
-	if dir == 3:
-		newAlbanianInstance.position.y = rng.randf_range(0, 500)
-		newAlbanianInstance.position.x = 1300
-	
-	newAlbanianInstance.dir = dir
-	self.add_child(newAlbanianInstance)
-	
-
-func _on_timer_timeout() -> void:
-	spawn_albanians()
-	$Timer.start()
