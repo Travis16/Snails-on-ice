368266b52a5a736a7360d109d3daae30e12fe70a
Joe Williams <jw06saturn@gmail.com>
2025-02-19 21:57:58 +0000

    first movement setup

--- scripts/albaniaMan.gd
+++ scripts/albaniaMan.gd
@@ -2,15 +2,15 @@ extends CharacterBody2D
 
 var rng = RandomNumberGenerator.new()
 var colour;
-var speed = 0;
-const rotation_speed = 3;
-const max_speed = 1000000
-const min_speed = 0
+var snail_force = 0;
+const rotation_snail_force = 3;
+const max_snail_force = 5000
+const min_snail_force = 0
 var forward_direction = Vector2.RIGHT
 var latest_collision
 var id
 var input_delay = 0.1
-var accel = 800
+var accel = 4000
 var brake = 6000
 
 # Called when the node enters the scene tree for the first time.
@@ -26,14 +26,26 @@ func _physics_process(delta: float) -> void:
 	forward_direction = Vector2.RIGHT.rotated(rotation)
 	
 	if Input.is_action_pressed("up" + id):
-		speed = clamp(speed + (accel * delta), min_speed, max_speed)
+		snail_force = clamp(snail_force + (accel * delta), min_snail_force, max_snail_force)
+		velocity = velocity.lerp(forward_direction*snail_force, delta*input_delay)
+	elif Input.is_action_pressed("down" + id):
+		snail_force = clamp(snail_force - (brake * delta), min_snail_force, max_snail_force)
+		velocity = velocity.lerp(forward_direction*snail_force, delta*input_delay)
+	else:
+		snail_force = min_snail_force
+		velocity = velocity.lerp(Vector2(0,0), delta * 0.5)
+		
+		
 	if Input.is_action_pressed("right" + id):
-		self.rotation_degrees += rotation_speed
-	if Input.is_action_pressed("down" + id):
-		speed = clamp(speed - (brake * delta), min_speed, max_speed)
+		self.rotation_degrees += rotation_snail_force
 	if Input.is_action_pressed("left" + id):
-		self.rotation_degrees -= rotation_speed
+		self.rotation_degrees -= rotation_snail_force
+		
+		
 	
-	speed = clamp(speed - 0.5, min_speed, max_speed)
 	
-	latest_collision = move_and_collide(lerp(self.velocity, forward_direction*speed, delta*input_delay)) 
+	
+	
+	
+	
+	var collision_info = move_and_collide(velocity*delta)
