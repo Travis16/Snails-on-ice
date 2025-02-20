b74abc8672f7abdf6ad7c747e21b71a6d2e22df7
Joe Williams <jw06saturn@gmail.com>
2025-02-20 00:36:49 +0000

    snails can now die

--- scripts/playerchar.gd
+++ scripts/playerchar.gd
@@ -12,9 +12,12 @@ var id
 const input_delay = 0.1
 var accel = 4000
 const bounce_force = 0.8
-var slistance = 200
+var slistance = 50
 var distance_moved = 0
 var slime_scene =  preload("res://scenes/trail_spot.tscn")
+const max_hp = 1
+var hp = max_hp
+var invincible = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -43,11 +46,9 @@ func _physics_process(delta: float) -> void:
 	var collision_info = move_and_collide(velocity*delta)
 	
 	if collision_info:
+		var what_was_hit = collision_info.get_collider()
 		snail_force = 0
 		velocity = velocity.bounce(collision_info.get_normal()) * bounce_force
-		
-		var what_was_hit = collision_info.get_collider()
-		
 		if "id" in what_was_hit:
 			what_was_hit.velocity = velocity.bounce(collision_info.get_normal()) * bounce_force
 	
@@ -62,9 +63,27 @@ func _physics_process(delta: float) -> void:
 			newslime.colour = 1
 		else:
 			newslime.colour = 2
+		newslime.creator = id
 		self.get_parent().add_child(newslime)
 		
+	var bodies = $trail_collider.get_overlapping_areas()
+	print(bodies)
+	for body in bodies:
+		if "creator" in body.get_parent():
+			if  body.get_parent().creator != id:
+				if invincible == 0:
+					hp -= 1
+					invincible = 60
+		
 	
+	if invincible > 0:
+		invincible -= 1
+		
+	if hp == 0:
+		self.queue_free()
+		
+	print(hp)
+
 	
 	
 	
--- scripts/trail_spot.gd
+++ scripts/trail_spot.gd
@@ -2,23 +2,25 @@ extends Node
 
 var rng = RandomNumberGenerator.new()
 var colour
+var creator
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if colour == 1:
-		$slime.modulate.r = rng.randf_range(0, 0.07)
-		$slime.modulate.g = rng.randf_range(0.9, 1)
+		$slime.modulate.r = rng.randf_range(0, 0.06)
+		$slime.modulate.g = rng.randf_range(0.7, 0.8)
 		$slime.modulate.b = 0
 	else:
-		$slime.modulate.r = rng.randf_range(0.6, 65)
+		$slime.modulate.r = rng.randf_range(0.5, 55)
 		$slime.modulate.g = 0
-		$slime.modulate.b = rng.randf_range(0, 0.07)
+		$slime.modulate.b = rng.randf_range(0, 0.06)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	$slime.modulate.a -= 0.01
-	
+	self.scale = self.scale * 0.999
+		
 	if $slime.modulate.a < 0:
 		self.queue_free()
 		
